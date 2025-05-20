ac36e8d9416e1519ade3cb8e1558f3c5ee2012fa
IStasovskyi93 <stsovsky400m@gmail.com>
2025-05-20 06:57:18 -0400

    create function & add  it to HTML

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,16 @@
+let length;
+let width;
+function calculateArea() {
+    length = parseFloat(document.getElementById("length").value);
+    width = parseFloat(document.getElementById("width").value);
+    let area = length * width;
+    document.getElementById("result").innerText = `The area of the rectangle is: ${area}`;
+}
+
+function grocerySum() {
+    let grocery1 = parseFloat(document.getElementById("grocery-1").value);
+    let grocery2 = parseFloat(document.getElementById("grocery-2").value);
+    let grocery3 = parseFloat(document.getElementById("grocery-3").value);
+    let amount = grocery1 + grocery2 + grocery3;
+    document.getElementById("groceryAmount").innerText = `The total amount is :$ ${amount}`;
+}
