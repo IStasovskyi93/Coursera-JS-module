f5bca7ff904c37688204b5f5e1289faa9793dc9b
IStasovskyi93 <stsovsky400m@gmail.com>
2025-05-26 12:45:18 -0400

    add fetchAPI

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,7 +1,23 @@
 function showweatherDetails(event) {
-      event.preventDefault();
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '8bc9d22b61a1b9600b289cf9c5552b62'; // Замените 'YOUR_API_KEY' на ваш фактический API ключ
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temperature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Ошибка при получении погоды:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Не удалось получить погоду. Пожалуйста, попробуйте снова.</p>`;
+    });
 }
 
- const city = document.getElementById('city').value;
-      const apiKey = '8bc9d22b61a1b9600b289cf9c5552b62'; // Замените 'YOUR_API_KEY' на ваш фактический API ключ
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
+ 
